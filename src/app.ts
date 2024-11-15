e88261a45b71e29c4568d07cf2456e057c09dd02
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-15 15:31:27 -0300

    validando e tratando erros nas validações e erros desconhecidos em ambiente de desenvolvimento

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,19 @@
+import { env } from '@/env'
 import { appRoutes } from '@/http/routes'
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
 app.register(appRoutes)
+app.setErrorHandler((error, _, reply) => {
+  if(error instanceof ZodError) {
+    return reply.status(400).send({ message: 'Validation error', issues: error.format() })
+  }
+  if(env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like Datadog/NewRelic/Sentry
+  }
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
