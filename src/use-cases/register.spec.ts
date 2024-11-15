bccec0dce3d52bfb1f869d14dad4a75a747fcd5d
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-15 16:25:02 -0300

    testando caso de uso

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,29 @@
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail() {
+        return null
+      },
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date()
+        }
+      }
+    })
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'KbYtM@example.com',
+      password: '123456'
+    })
+    const isPasswordCorrectHashed = await compare('123456', user.password_hash)
+    expect(isPasswordCorrectHashed).toBe(true)
+  })
+})
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,11 +1,15 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
+}
+interface RegisterUseCaseResponse {
+  user: User
 }
 export class RegisterUseCase {
   
@@ -15,7 +19,7 @@ export class RegisterUseCase {
     name,
     email,
     password
-  }: RegisterUseCaseRequest) {
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
   
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -24,10 +28,11 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
     
-    await this.usersRepository.create({
+    const user =await this.usersRepository.create({
       name,
       email,
       password_hash
     })
+    return {user}
   }
 }
