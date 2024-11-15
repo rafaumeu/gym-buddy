2efbe7d44efcfb197ab20d7f27f281ba69170db5
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-15 08:30:00 -0300

    criando relacionamentos

--- prisma/migrations/20241115111306_create_gyms_and_check_ins/migration.sql
+++ prisma/migrations/20241115111306_create_gyms_and_check_ins/migration.sql
@@ -5,26 +5,29 @@
 
 */
 -- AlterTable
-ALTER TABLE "users" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ALTER TABLE "users" ADD COLUMN     "created_at" TIMESTAMP
+(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN     "password_hash" TEXT NOT NULL;
 
 -- CreateTable
-CREATE TABLE "check_ins" (
-    "id" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "validated_at" TIMESTAMP(3),
+CREATE TABLE "check_ins"
+(
+  "id" TEXT NOT NULL,
+  "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "validated_at" TIMESTAMP(3),
 
-    CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
+  CONSTRAINT "check_ins_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "gyms" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "phone" TEXT,
-    "latitude" DECIMAL(65,30) NOT NULL,
-    "longitude" DECIMAL(65,30) NOT NULL,
+CREATE TABLE "gyms"
+(
+  "id" TEXT NOT NULL,
+  "title" TEXT NOT NULL,
+  "description" TEXT,
+  "phone" TEXT,
+  "latitude" DECIMAL(65,30) NOT NULL,
+  "longitude" DECIMAL(65,30) NOT NULL,
 
-    CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
+  CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
 );
