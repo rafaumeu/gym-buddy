a6a61122e9f576275024dad9144243d013036cae
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-15 11:29:19 -0300

    hash de senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+
 
 export async function register(request: FastifyRequest, reply: FastifyReply): Promise<void> {
   const registerBodySchema = z.object({
@@ -10,11 +12,24 @@ export async function register(request: FastifyRequest, reply: FastifyReply): Pr
   })
 
   const {name, email, password} = registerBodySchema.parse(request.body)
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if(userWithSameEmail) {
+    return reply.status(409).send({
+      message: 'User already exists'
+    })
+  }
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password
+      password_hash
     }
   })
 
