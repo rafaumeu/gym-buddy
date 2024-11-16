1e288267b50641049d31c0f25dbaef13df8ab44b
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-16 12:12:08 -0300

    factory pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,32 +1,32 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
-import { RegisterUseCase } from '@/use-cases/register'
-import { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists"
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case"
+import { FastifyReply, FastifyRequest } from "fastify"
+import { z } from "zod"
 
-
-export async function register(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+export async function register(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<void> {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
   })
 
-  const {name, email, password} = registerBodySchema.parse(request.body)
+  const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const registerUseCase = makeRegisterUseCase()
     await registerUseCase.execute({
       name,
       email,
-      password
+      password,
     })
   } catch (error) {
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message })
     }
-    throw error 
+    throw error
   }
 
   return reply.status(201).send()
---
+++ src/use-cases/factories/make-authenticate-use-case.ts
@@ -0,0 +1,8 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
+import { AuthenticateUseCase } from "@/use-cases/authenticate"
+
+export function makeAuthenticateUseCase() {
+  const usersRepository = new PrismaUsersRepository()
+  const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+  return authenticateUseCase
+}
