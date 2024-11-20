2b94be09c0bb34bebaaec9ce51d9130a4f088251
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-20 15:27:20 -0300

    criação do profile controller, e do middleware verify jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,10 +1,11 @@
 import { authenticate } from '@/http/controllers/authenticate'
 import { profile } from '@/http/controllers/profile'
 import { register } from '@/http/controllers/register'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
