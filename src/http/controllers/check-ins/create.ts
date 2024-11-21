a1a89a4d71c1b14021334642af4febdc165ee686
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-21 12:50:07 -0300

    conclusão dos testes e2e controller checkIn

--- src/http/controllers/check-ins/create.ts
+++ src/http/controllers/check-ins/create.ts
@@ -3,9 +3,10 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
-  const createCheckInsParamsSchema = z.object({
+  const createCheckInParamsSchema = z.object({
     gymId: z.string().uuid(),
   })
+
   const createCheckInBodySchema = z.object({
     latitude: z.number().refine((value) => {
       return Math.abs(value) <= 90
@@ -14,16 +15,18 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
       return Math.abs(value) <= 180
     }),
   })
-  const { gymId } = createCheckInsParamsSchema.parse(request.params)
+
+  const { gymId } = createCheckInParamsSchema.parse(request.params)
   const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
 
-  const createCheckInUseCase = makeCheckInUseCase()
-  await createCheckInUseCase.execute({
+  const checkInUseCase = makeCheckInUseCase()
+
+  await checkInUseCase.execute({
     gymId,
     userId: request.user.sub,
     userLatitude: latitude,
     userLongitude: longitude,
   })
-
+  console.log(checkInUseCase)
   return reply.status(201).send()
 }
--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,8 +1,9 @@
+import { create } from '@/http/controllers/check-ins/create'
 import { history } from '@/http/controllers/check-ins/history'
 import { metrics } from '@/http/controllers/check-ins/metrics'
 import { validate } from '@/http/controllers/check-ins/validate'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
-import { create } from 'domain'
+
 import { FastifyInstance } from 'fastify'
 
 export async function checkInsRoutes(app: FastifyInstance) {
