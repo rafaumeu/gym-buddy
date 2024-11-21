9df17e52dd7dd282615895523b4b74bf22dfadb1
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-21 05:17:16 -0300

    configurando test environment prisma

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environmentMatchGlobs: [
-      [
-        './src/http/controllers/**',
-        './prisma/vitest-environment-prisma/prisma-test-environment.ts',
-      ],
-    ],
+    environmentMatchGlobs: [['./src/http/controllers/**', 'prisma']],
     dir: 'src',
   },
 })
