99bb91f76e8ccd36339b3ec059dd3e4ca5889d3f
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-11-21 21:48:27 -0300

    configurando o refreshToken

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,23 @@ import { env } from '@/env'
 import { checkInsRoutes } from '@/http/controllers/check-ins/routes'
 import { gymsRoutes } from '@/http/controllers/gyms/routes'
 import { usersRoutes } from '@/http/controllers/users/routes'
+import fastifyCookies from '@fastify/cookie'
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
-
 export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: { expiresIn: '10m' },
 })
 app.register(usersRoutes)
 app.register(gymsRoutes)
 app.register(checkInsRoutes)
+app.register(fastifyCookies)
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
